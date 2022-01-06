4da64f67f6321fa1de1f6a248083e09511afbf68
theWituch <thewituch@gmail.com>
2022-01-06 04:52:29 +0100

    Add translation keys to single-update pages

--- language/en.php
+++ language/en.php
@@ -1,12 +1,9 @@
 <?php
 return array( // {{ 'my_key'|translate }}
-	"header.motto" => "sda",
-	
 
     "fotter.interested" => 'Interested?',
     "fotter.discord" => 'Visit us on',
     "fotter.github" => 'Also check out our',
-
 
     "updates.released" => "Updates released",
     "updates.released.desc" => "The list of released updates includes those that have already been implemented for MC + and are available for download. Displayed in order from newest to oldest.",
@@ -17,5 +14,9 @@ return array( // {{ 'my_key'|translate }}
     "updates.proposed" => "Proposed updates",
     "updates.proposed.desc" => "Below is a list of updates that may or may not be implemented in MC +. Proposed updates are based on the overall concept and relationship with released or scheduled updates.",
 
-	"updates.empty" => "No updates to display",
+    "updates.empty" => "No updates to display",
+
+    "update-page.dependencies" => "Dependencies with other modifications",
+    "update-page.dependent" => "Depends on",
+    "update-page.extension" => "Complementary for",
 );
--- language/pl.php
+++ language/pl.php
@@ -1,12 +1,9 @@
 <?php
 return array( // {{ 'my_key'|translate }}
-	"header.motto" => "sda",
-	
 
     "fotter.interested" => 'Zainteresowany?',
     "fotter.discord" => 'Odwiedź nasz',
     "fotter.github" => 'Zajrzyj również na nasz',
-
 
     "updates.released" => "Aktualizacje wydane",
     "updates.released.desc" => "Lista aktualizacji wydanych, zawiera te które zostały już zaimplementowane do MC+ i są dostępne do pobrania. Wyświetlane w kolejności od najnowszej do najstarszej.",
@@ -17,6 +14,10 @@ return array( // {{ 'my_key'|translate }}
     "updates.proposed" => "Aktualizacje proponowane",
     "updates.proposed.desc" => "Poniżej znajduje się lista aktualizacji, które mogą zostać zaimplementowane do MC+ ale nie muszą. Proponowane aktualizacje są podparte ogólną koncepcją i relacjami z wydanymi lub zaplanowanymi aktualizacjami.",
 
-	"updates.empty" => "Brak aktualizacji do wyświetlenia",
+    "updates.empty" => "Brak aktualizacji do wyświetlenia",
+
+    "update-page.dependencies" => "Zależności z innymi modyfikacjami",
+    "update-page.dependent" => "Zależna od",
+    "update-page.extension" => "Uzupełniająca dla",
 
 );
