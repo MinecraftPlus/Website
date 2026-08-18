d7a8b523d0415eba492d750c659bd39535d3f6e6
theWituch <thewituch@gmail.com>
2026-08-18 14:23:20 +0200

    Add support for HTTPS behind a proxy server in index.php

--- index.php
+++ index.php
@@ -21,6 +21,12 @@ if (!extension_loaded('mbstring')) {
     die("Pico requires the PHP extension 'mbstring' to run");
 }
 
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
 // load dependencies
 require_once(__DIR__ . '/vendor/autoload.php');
 
